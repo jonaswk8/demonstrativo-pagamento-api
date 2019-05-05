03d82e1556ec3a48fb9a97497b7cab891fa89d49
Liax - Jonas Fernandes <jonas.fernandes@parceiros.odontoprev.com.br>
2019-05-05 17:23:53 -0300

    Commit inicial API demonstrativo de pagamento

---
+++ README.md
@@ -0,0 +1,1 @@
+"# demonstrativo-pagamento-api" 
---
+++ src/main/java/br/com/odontoprev/api/ApiDemonstrativoPagamentoApplication.java
@@ -0,0 +1,14 @@
+package br.com.odontoprev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemonstrativoPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemonstrativoPagamentoApplication.class, args);
+		
+	}
+
+}
